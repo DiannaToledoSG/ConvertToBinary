37d73238ce48c660efb3af5e36099d9c1c7bb4e3
Dianna A Toledo <56136290+DiannaToledoSG@users.noreply.github.com>
2019-11-19 18:52:24 -0800

    Add files via upload

---
+++ cs160_wk9_lc1/src/edu/cgcc/cs160/people/Instructor.java
@@ -0,0 +1,8 @@
+package edu.cgcc.cs160.people;
+
+public class Instructor extends Person {
+
+	int pay;
+	boolean vacation;
+	
+}
---
+++ cs160_wk9_lc1/src/edu/cgcc/cs160/people/Student.java
@@ -0,0 +1,18 @@
+package edu.cgcc.cs160.people;
+
+public class Student extends Person {
+		//members
+	public int gpa;
+	public int loanBalance;
+
+		//constructor
+	public Student(String n, int a, int h) {
+		name = n;
+		age = a;
+		height = h;
+	}
+	//member functions
+	public void printInformation() {
+		System.out.println(name + ", " + height +", "+ age + ", " + gpa);
+}
+}
